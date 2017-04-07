8907cbf20d2001c279598ef22292580ec73f0d6e
GiantTree <webmaster@gianttree.de>
2017-04-07 21:33:11 +0200

    Add more documentation.

--- src/main/java/com/github/gianttreelp/bukkitdependencyloader/DependencyLoaderPlugin.java
+++ src/main/java/com/github/gianttreelp/bukkitdependencyloader/DependencyLoaderPlugin.java
@@ -2,17 +2,18 @@ package com.github.gianttreelp.bukkitdependencyloader;
 
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.eclipse.aether.artifact.Artifact;
 
 /**
  * This {@link Plugin} offers a way to get a {@link DependencyLoader} for your plugin
  * and use it to dynamically load {@link org.eclipse.aether.artifact.Artifact}s at runtime
  * without adding them to the jar.
- *
+ * <p>
  * This allows other plugins to reuse artifacts and in turn reduces the file size
  * of many plugins.
- *
+ * <p>
  * Example usage:
- *
+ * <p>
  * <code>
  * // Loads the kotlin runtime into the classpath
  * DependencyLoader loader = DependencyLoaderPlugin.forPlugin(this);
@@ -25,8 +26,15 @@ public class DependencyLoaderPlugin extends JavaPlugin {
     /**
      * Empty constructor, we don't need to do anything here.
      */
-    public DependencyLoaderPlugin() {}
+    public DependencyLoaderPlugin() {
+    }
 
+    /**
+     * Creates a new {@link DependencyLoader} for a given {@link Plugin}
+     *
+     * @param plugin the plugin that requested the dependency loader
+     * @return a new instance of {@link DependencyLoader}
+     */
     public static DependencyLoader forPlugin(JavaPlugin plugin) {
         return new DependencyLoader(plugin);
     }
@@ -39,6 +47,12 @@ public class DependencyLoaderPlugin extends JavaPlugin {
     }
 
 
+    /**
+     * Loads its own {@link Artifact}s as a way to test the connection and to provide those artifacts completely,
+     * because some functionality might be lost when minifying the plugin.
+     * <p>
+     * It is known that none of these artifacts are loaded because we are the first plugin to actually load anything.
+     */
     private void loadOwnArtifacts() {
         DependencyLoader loader = DependencyLoaderPlugin.forPlugin(this);
         loader.loadArtifact("org.eclipse.aether", "aether-impl", "1.1.0");
